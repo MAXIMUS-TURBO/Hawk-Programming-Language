5ade5ec397d998919d6eaec65a6c80aab5c01602
Fatima <119454099+MAXIMUS-TURBO@users.noreply.github.com>
2025-09-25 10:08:29 -0500

    Add files via upload

---
+++ Hawk/src/parser.java
@@ -0,0 +1,16 @@
+/***********************************************************
+
+This scanner is like the lex analyzer of the program
+>keep track of line numbers
+>unusual identifiers
+    -redeclared
+    -undeclared
+>illegal number or symbol
+>IMPORTANT: store all declared variables in a symbol table
+
+***********************************************************/
+
+
+public class parser {
+    
+}
