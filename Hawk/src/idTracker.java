4085002767421df4b34b4d45d194c2f02fc9a4c6
Fatima <119454099+MAXIMUS-TURBO@users.noreply.github.com>
2025-09-25 10:41:40 -0500

    Update and rename symbol_table.java to idTracker.java

---
+++ Hawk/src/idTracker.java
@@ -0,0 +1,10 @@
+/***********************************************************
+
+This program will deploy a symbol table that will track 
+    identifiers that have been declared
+
+***********************************************************/
+
+public class idTracker {
+    
+}
--- Hawk/src/symbol_table.java
+++
@@ -1,3 +0,0 @@
-public class symbol_table {
-    
-}
